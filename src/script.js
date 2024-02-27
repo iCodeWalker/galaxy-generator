fb9e36106aa7604fe2ecdc0ae6381a5e20a0db8d
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-02-28 00:33:34 +0530

    added : spin and randomness in the galaxy

--- src/script.js
+++ src/script.js
@@ -26,6 +26,12 @@ parameters.size = 0.01;
 parameters.radius = 5;
 // branches of galaxy
 parameters.branches = 3;
+// for spin
+parameters.spin = 1;
+// for randomness
+parameters.randomness = 0.2;
+
+parameters.randomnessPower = 3;
 
 let geometry = null;
 let material = null;
@@ -65,10 +71,32 @@ const generateGalaxy = () => {
     const radius = Math.random() * parameters.radius;
     const branchAngle =
       ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+    // spin angle for spinning the particles. The farther the particle the more it spins
+    const spinAngle = radius * parameters.spin;
 
-    positions[i3] = Math.cos(branchAngle) * radius;
-    positions[i3 + 1] = 0;
-    positions[i3 + 2] = Math.sin(branchAngle) * radius;
+    // const randomX = (Math.random() - 0.5) * parameters.randomness * radius;
+    // const randomY = (Math.random() - 0.5) * parameters.randomness * radius;
+    // const randomZ = (Math.random() - 0.5) * parameters.randomness * radius;
+
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+
+    positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
   }
 
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
@@ -125,6 +153,24 @@ gui
   .min(2)
   .max(20)
   .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "spin")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "randomness")
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "randomnessPower")
+  .min(1)
+  .max(10)
+  .step(0.001)
   .onFinishChange(generateGalaxy);
 
 /**
