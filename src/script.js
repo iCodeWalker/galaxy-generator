db73fba010edfb038ab32c9bf0928f4a080a9d7e
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-02-27 22:47:34 +0530

    added : particles in straight line

--- src/script.js
+++ src/script.js
@@ -22,6 +22,8 @@ const scene = new THREE.Scene();
 const parameters = {};
 parameters.count = 100000;
 parameters.size = 0.01;
+// create a paranter radius for making spiral galaxy
+parameters.radius = 5;
 
 let geometry = null;
 let material = null;
@@ -47,9 +49,15 @@ const generateGalaxy = () => {
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
 
-    positions[i3] = (Math.random() - 0.5) * 3;
-    positions[i3 + 1] = (Math.random() - 0.5) * 3;
-    positions[i3 + 2] = (Math.random() - 0.5) * 3;
+    // positions[i3] = (Math.random() - 0.5) * 3;
+    // positions[i3 + 1] = (Math.random() - 0.5) * 3;
+    // positions[i3 + 2] = (Math.random() - 0.5) * 3;
+
+    // Potioning particles on straight line
+    const radius = Math.random() * parameters.radius;
+    positions[i3] = radius;
+    positions[i3 + 1] = 0;
+    positions[i3 + 2] = 0;
   }
 
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
@@ -94,6 +102,12 @@ gui
   .min(0.001)
   .max(0.1)
   .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
   .onFinishChange(generateGalaxy);
 
 /**
